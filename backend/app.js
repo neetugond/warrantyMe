962535d82f53699e57ef94641a292d087cd27f5d
neetugond <93507242+neetugond@users.noreply.github.com>
2022-07-20 16:06:06 +0530

    server created

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express();
+
+app.get('/',(req, res)=> {
+    res.send("hello world")
+})
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+    console.log(`server is running at port ${PORT}`)
+})
